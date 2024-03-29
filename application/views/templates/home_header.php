e2cfa899c67db9ee259a009f4240824deab6b8df
braverious <muhammadraihan877@gmail.com>
2024-03-29 21:52:06 +0700

    Memperbaiki home dan headline

--- application/views/templates/home_header.php
+++ application/views/templates/home_header.php
@@ -12,8 +12,9 @@
     </script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Quicksand:wght@300..700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/home/index.css">
+    <style>
+    </style>
 </head>
