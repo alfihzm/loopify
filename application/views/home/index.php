cc0c8019939482aa90853ad7c9061658d1ccb9fc
Ducckyys <Andriannfakih@gmail.com>
2024-04-07 12:30:34 +0700

    Memperbaiki Landing Page pada bagian tulisan

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,5 +1,5 @@
 <body>
-    <nav style="height: 14vh;" class="navbar navbar-expand-lg fixed-top" class="navbar navbar-expand-lg
+    <nav style="height: 10vh;" class="navbar navbar-expand-lg fixed-top" class="navbar navbar-expand-lg
         bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand ms-5" href="home">
@@ -7,9 +7,6 @@
                     src="assets/images/logo/branding-logo.png">
             </a>
 
-            <!-- <a class="navbar-brand ms-5" href="home">
-                Recyloop.id
-            </a> -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -34,50 +31,27 @@
                     </li>
                     <li class="nav-item">
                         <a style="margin-right: 50px; font-size: 18px; font-weight: 400; font-family: Poppins;"
-                            class="nav-link" href="auth">Login</a>
+                            class="nav-link" href="auth">Masuk</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
     <section class="content">
-        <!-- <div class="cover">A</div>
-        <div style="width: 100%; height: 100%; position: relative">
-            <div class="headline"
-                style="width: 152px; height: 65px; left: 214px; top: 255px; position: absolute; background: #80E2ED; border-radius: 10px; z-index: 1;">
-            </div>
-            <div class="headline"
-                style="width: 60px; height: 30px; left: 46px; top: 273px; position: absolute; color: white; font-size: 18px; font-family: Poppins; font-weight: 700; word-wrap: break-word; z-index: 1;">
-                Daftar</div>
-            <div class="headline"
-                style="width: 152px; height: 65px; left: 0px; top: 255px; position: absolute; background: rgba(1, 40, 60, 0); border-radius: 10px; border: 1px #80E2ED solid; z-index: 1;">
-            </div>
-            <div class="headline"
-                style="width: 87px; height: 23px; left: 246px; top: 276px; position: absolute; color: white; font-size: 16px; font-family: Poppins; font-weight: 700; word-wrap: break-word; z-index: 1;">
-                Tukar Poin</div>
-            <div class="headline"
-                style="width: 536px; height: 54px; left: 0px; top: 165px; position: absolute; justify-content: center; align-items: center; display: inline-flex; z-index: 1;">
-                <div
-                    style="width: 536px; height: 54px; color: white; font-size: 18px; font-family: Poppins; font-weight: 400; word-wrap: break-word">
-                    Lestarikan alam dengan mengurangi angka dengan limbah</div>
-            </div>
-            <div
-                style="left: 0px; top: 0px; position: absolute; color: white; font-size: 24px; font-family: Poppins; font-weight: 600; word-wrap: break-word; z-index: 1;">
-                Hai, Selamat Datang</div>
-            <div
-                style="width: 576px; height: 72px; left: 0px; top: 27px; position: absolute; color: white; font-size: 48px; font-family: Poppins; font-weight: 800; word-wrap: break-word; z-index: 1;">
-                Tukarkan sampah dengan tunai</div>
-        </div> -->
+        <div class="cover">
+                <div style="width: 100%; height: 100%; position: relative">
+                    <a class="head2" href="#">
+                        Tukar Poin
+                    </a>
+                    <a class="head3" href="#">
+                        Daftar
+                    </a>
+                    <div class="head">
+                        <div class="p3"> Lestarikan alam dengan mengurangi angka dengan limbah</div>
+                    </div>
+                    <div class="sapa"> Hai, Selamat Datang</div>
+                    <div class="p2"> Tukarkan sampah dengan tunai</div>
+                </div>
+        </div>
         <img class="bg-image" src="assets/images/background/recyloopbg.jpeg" alt="background">
-    </section>
-
-    <!-- <div class="container-fluid">
-            <div class="logo-content">
-                <img class="logo" src="assets/images/logo/main-logo.png" alt="logo">
-            </div> 
-            <div class="desc">
-                <h1>Hai, selamat datang!</h1>
-            </div> 
-        </div> -->
-
     </section>
