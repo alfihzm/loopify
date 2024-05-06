6001bcf9fe4584f5b2cc194250756de075f38378
alfihzm <alfihzm@gmail.com>
2024-05-06 13:24:35 +0700

    new database

--- recyloop.sql
+++ recyloop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2024 at 10:37 AM
+-- Generation Time: May 06, 2024 at 08:23 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -19,6 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `recyloop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cinderamata`
+--
+
+CREATE TABLE `cinderamata` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `harga` int(64) NOT NULL,
+  `deskripsi` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,7 +55,7 @@ CREATE TABLE `company` (
 --
 
 CREATE TABLE `member` (
-  `id` int(11) NOT NULL,
+  `id_member` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
   `lahir` date NOT NULL,
   `no_telp` varchar(16) NOT NULL,
@@ -73,11 +86,19 @@ CREATE TABLE `poin` (
 
 CREATE TABLE `sampah` (
   `id` int(11) NOT NULL,
-  `jenis` varchar(64) NOT NULL,
-  `nilai_tukar` int(64) NOT NULL,
-  `qty` int(11) NOT NULL,
-  `berat` int(64) NOT NULL
+  `jenis_sampah` varchar(64) NOT NULL,
+  `nilai_tukar` int(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sampah`
+--
+
+INSERT INTO `sampah` (`id`, `jenis_sampah`, `nilai_tukar`) VALUES
+(1, 'Kantong Plastik', 100),
+(2, 'Botol Kaca', 500),
+(4, 'Botol Plastik', 150),
+(5, 'Sampah Daun', 50);
 
 -- --------------------------------------------------------
 
@@ -86,13 +107,28 @@ CREATE TABLE `sampah` (
 --
 
 CREATE TABLE `staff` (
-  `id` int(11) NOT NULL,
+  `id_staff` int(11) NOT NULL,
   `nama` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `username` varchar(16) NOT NULL,
   `alamat` varchar(256) NOT NULL,
-  `no_telp` int(11) NOT NULL,
-  `role` int(11) NOT NULL,
+  `no_telp` varchar(16) NOT NULL,
+  `photo` varchar(128) NOT NULL,
+  `role_id` int(1) NOT NULL,
   `is_active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id_staff`, `nama`, `email`, `username`, `alamat`, `no_telp`, `photo`, `role_id`, `is_active`) VALUES
+(10240001, 'Derby Markus', 'derbymarkus@gmail.com', 'derby', 'Bandung', '082173958206', 'default.jpg', 2, 1),
+(10240002, 'Derby Fransiskus', 'derby@gmail.com', 'derby', 'Bandung', '082173958206', 'default.jpg', 2, 1),
+(10240004, 'Dendi Rahmat', 'dendi@gmail.com', 'dendi', 'Lebak', '082175927592', 'default.jpg', 2, 1),
+(10240005, 'Adam Warlock', 'adam@gmail.com', 'adam', 'Manchester', '082174927592', 'default.jpg', 2, 1),
+(10240006, 'Agus Susanto', 'agus@gmail.com', 'agus', 'Pandeglang', '082173952058', 'default.jpg', 2, 1),
+(10240007, 'Damar Aji', 'damar@gmail.com', 'damar', 'Sukabumi', '082186729673', 'default.jpg', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -102,6 +138,9 @@ CREATE TABLE `staff` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `id_staff` int(11) DEFAULT NULL,
+  `id_member` int(11) DEFAULT NULL,
   `nama` varchar(128) NOT NULL,
   `lahir` date NOT NULL,
   `email` varchar(128) NOT NULL,
@@ -121,10 +160,13 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `nama`, `lahir`, `email`, `username`, `password`, `role_id`, `photo`, `no_telp`, `alamat`, `level`, `poin`, `date_created`, `is_active`) VALUES
-(1, 'Beta', '1999-01-01', 'betara@gmail.com', 'betara', '$2y$10$FLuzN7X92R2TQFttbmiQDOacjuIKMV8NnqPrjLrGdU.ow1DNxVEhu', 3, 'default.jpg', '082161872392', 'Jakarta Selatan', 1, 0, 1712325370, 1),
-(2, 'Mohammad Alfi Hamzami', '1999-11-29', 'alfihzm@gmail.com', 'alfihzm', '$2y$10$B4vdInCWbU84dVQz9XPTr.UDJZ/Ex3lQ2Zr76i/vHb/NMoaKLgxPG', 1, 'default.jpg', '082161872392', 'Kabupaten Tangerang', 1, 0, 1712465510, 1),
-(3, 'Ferdy Kurniawan', '2004-02-28', 'ferdi@gmail.com', 'ferdiku', '$2y$10$2M6uxQeT8w3x7KOeG9WZJO4H5KisObvJW.UtsISD3v2YkUGszowfu', 2, 'default.jpg', '082887722147', 'Tangerang Selatan', 0, 0, 1714328337, 1);
+INSERT INTO `user` (`id`, `id_admin`, `id_staff`, `id_member`, `nama`, `lahir`, `email`, `username`, `password`, `role_id`, `photo`, `no_telp`, `alamat`, `level`, `poin`, `date_created`, `is_active`) VALUES
+(1, NULL, NULL, 10480001, 'Beta', '1999-01-01', 'betara@gmail.com', 'betara', '$2y$10$FLuzN7X92R2TQFttbmiQDOacjuIKMV8NnqPrjLrGdU.ow1DNxVEhu', 3, 'default.jpg', '082161872392', 'Jakarta Selatan', 1, 0, 1712325370, 1),
+(2, 19220821, NULL, NULL, 'Mohammad Alfi Hamzami', '1999-11-29', 'alfihzm@gmail.com', 'alfihzm', '$2y$10$B4vdInCWbU84dVQz9XPTr.UDJZ/Ex3lQ2Zr76i/vHb/NMoaKLgxPG', 1, 'user_Mohammad_Alfi_Hamzami.jpg', '082161872392', 'Kabupaten Tangerang', 1, 0, 1712465510, 1),
+(6, NULL, 10240001, NULL, 'Derby Sucipto', '2005-05-11', 'derby@gmail.com', 'derby', '$2y$10$o1uWEO7QFLgEyQ3wqSbCAu1kAwjxfYa9/RJ3MJosDtN5NrZmCEX6C', 2, 'default.jpg', '082173958206', 'Bandung', 1, 0, 1714830415, 1),
+(7, NULL, 10240004, NULL, 'Dendi Rahmat', '2006-11-04', 'dendi@gmail.com', 'dendi', '$2y$10$lQlCdblquou221pGTm3MDudqMSSENy17VBjUvcr7MTgrDFqCDUeUu', 2, 'default.jpg', '082175927592', 'Lebak', 1, 0, 1714840913, 1),
+(8, NULL, 10240005, NULL, 'Adam Warlock', '2004-04-04', 'adam@gmail.com', 'adam', '$2y$10$PsSx5kVJY/otqt2CNtqiCeV.cyEJSbzGVzDo2OQF0vJLjHe9VAHLq', 2, 'default.jpg', '082174927592', 'Manchester', 1, 0, 1714841969, 1),
+(9, NULL, 10240006, NULL, 'Agus Susanto', '2004-04-24', 'agus@gmail.com', 'agus', '$2y$10$tgW2yVQhnKz9scSIv1QcC..v0UZa7nQvgCPmXzJtnckuqbm0uKbdG', 2, 'default.jpg', '082173952058', 'Pandeglang', 1, 0, 1714842198, 1);
 
 -- --------------------------------------------------------
 
@@ -146,9 +188,11 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (1, 1, 1),
 (2, 1, 2),
 (3, 1, 3),
-(4, 2, 2),
-(5, 2, 4),
-(6, 3, 3);
+(4, 1, 5),
+(5, 1, 6),
+(6, 2, 2),
+(7, 2, 4),
+(8, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -168,7 +212,10 @@ CREATE TABLE `user_menu` (
 INSERT INTO `user_menu` (`id`, `menu`) VALUES
 (1, 'Dashboard'),
 (2, 'Pengaturan User'),
-(3, 'Cek Sampah');
+(3, 'Manajemen Staff'),
+(4, 'Manajemen Anggota'),
+(5, 'Manajemen Sampah'),
+(6, 'Manajemen Cinderamata');
 
 -- --------------------------------------------------------
 
@@ -213,14 +260,22 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-home', 1),
 (2, 2, 'Profil Saya', 'admin/my_profile', 'fas fa-fw fa-user', 1),
 (3, 2, 'Ubah Profil', 'admin/edit_profile', 'fas fa-solid fa-user-pen', 1),
-(4, 1, 'Tambah Staff', 'admin/add_staff', 'fas fa-fw fa-user-plus', 1),
-(5, 3, 'Informasi Staff', 'admin/staff_info', 'fas fa-fw fa-id-badge', 1),
-(6, 2, 'Informasi Anggota', 'staff/informasi-anggota', 'fas fa-fw fa-users', 1),
-(7, 4, 'Tambah Anggota', 'staff/add_member', 'fas fa-fw fa-person-circle-plus', 1);
+(4, 3, 'Manajemen Staff', 'admin/staff', 'fas fa-fw fa-id-badge', 1),
+(5, 3, 'Tambah Staff', 'admin/add_staff', 'fas fa-fw fa-user-plus ', 0),
+(6, 4, 'Informasi Anggota', 'staff/informasi-anggota', 'fas fa-fw fa-users', 1),
+(7, 4, 'Tambah Anggota', 'staff/add_member', 'fas fa-fw fa-person-circle-plus', 1),
+(8, 5, 'Manajamen Sampah', 'admin/sampah', 'fas fa-fw fa-trash-can', 1),
+(9, 6, 'Manajemen Cinderamata', 'admin/manajemen_cinderamata', 'fas fa-fw fa-gift', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cinderamata`
+--
+ALTER TABLE `cinderamata`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `company`
@@ -232,13 +287,25 @@ ALTER TABLE `company`
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_member`);
 
 --
 -- Indexes for table `poin`
 --
 ALTER TABLE `poin`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sampah`
+--
+ALTER TABLE `sampah`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id_staff`);
 
 --
 -- Indexes for table `user`
@@ -275,6 +342,11 @@ ALTER TABLE `user_sub_menu`
 --
 
 --
+-- AUTO_INCREMENT for table `cinderamata`
+--
+ALTER TABLE `cinderamata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
@@ -283,27 +355,32 @@ ALTER TABLE `company`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `poin`
 --
 ALTER TABLE `poin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `sampah`
+--
+ALTER TABLE `sampah`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `user_role`
 --
@@ -313,7 +390,7 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
