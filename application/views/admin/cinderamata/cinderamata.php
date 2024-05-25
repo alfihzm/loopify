eec53e401a336d27b21f25713ad1e1ff3595c061
alfihzm <alfihzm@gmail.com>
2024-05-25 18:47:58 +0700

    Adding sequence number for table cinderamata..

--- application/views/admin/cinderamata/cinderamata.php
+++ application/views/admin/cinderamata/cinderamata.php
@@ -19,6 +19,7 @@
                 <table class="table">
                     <thead>
                         <tr>
+                            <th scope="col" style="width: 50px;">No.</th>
                             <th scope="col" style="width: 100px;">Nama</th>
                             <th scope="col" style="width: 100px;">Harga</th>
                             <th scope="col" style="width: 200px;">Deskripsi</th>
@@ -27,19 +28,27 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($cinderamata as $c) : ?>
-                            <tr>
-                                <td><?= $c['nama_gift']; ?></td>
-                                <td><?= $c['harga']; ?> poin</td>
-                                <td><?= $c['deskripsi']; ?></td>
-                                <td>
-                                    <img class="mb-2 mt-2" style="border: 1px solid #FFF; border-radius: 5px;" src="<?= base_url('assets/images/cinderamata/' . $c['photo']); ?>" alt="Gambar" width="85" height="85">
-                                </td>
-                                <td>
-                                    <a href="<?= base_url('admin/ubah_gift/'   . $c['id']); ?>" class="btn btn-success btn-sm"><i style="color: #000;" class="fa-solid fa-pencil"></i></a>
-                                    <a href="<?= base_url('admin/hapus_gift/' . $c['id']); ?>" class="btn btn-danger btn-sm"><i style="color: #000;" class="fa-solid fa-trash"></i></a>
-                                </td>
-                            </tr>
+                        <?php $i = 1;
+                        foreach ($cinderamata as $c) : ?>
+                        <tr>
+                            <td><?= $i++; ?></td>
+                            <td><?= $c['nama_gift']; ?></td>
+                            <td><?= $c['harga']; ?> poin</td>
+                            <td><?= $c['deskripsi']; ?></td>
+                            <td>
+                                <img class="mb-2 mt-2" style="border: 1px solid #FFF; border-radius: 5px;"
+                                    src="<?= base_url('assets/images/cinderamata/' . $c['photo']); ?>" alt="Gambar"
+                                    width="85" height="85">
+                            </td>
+                            <td>
+                                <a href="<?= base_url('admin/ubah_gift/'   . $c['id']); ?>"
+                                    class="btn btn-success btn-sm"><i style="color: #000;"
+                                        class="fa-solid fa-pencil"></i></a>
+                                <a href="<?= base_url('admin/hapus_gift/' . $c['id']); ?>"
+                                    class="btn btn-danger btn-sm"><i style="color: #000;"
+                                        class="fa-solid fa-trash"></i></a>
+                            </td>
+                        </tr>
                         <?php endforeach; ?>
                     </tbody>
                 </table>
@@ -49,9 +58,9 @@
     </div>
 
     <script type="text/javascript">
-        window.setTimeout(function() {
-            $(".col-lg-6").fadeTo(500, 0).slideUp(500, function() {
-                $(this).remove();
-            });
-        }, 2000);
+    window.setTimeout(function() {
+        $(".col-lg-6").fadeTo(500, 0).slideUp(500, function() {
+            $(this).remove();
+        });
+    }, 2000);
     </script>
