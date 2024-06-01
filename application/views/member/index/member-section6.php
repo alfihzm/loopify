0dbe0bc3751cb13d98220486642767f4673799d0
alfihzm <alfihzm@gmail.com>
2024-06-02 05:37:47 +0700

    Minor changes..

--- application/views/member/index/member-section6.php
+++ application/views/member/index/member-section6.php
@@ -1,57 +1,60 @@
-        <section class="section">
-            <div class="container">
-                <div class="heading-section">
-                    <div class="heading-subtitle"><span class="tx-primary tx-16 fw-semibold">Produk Akhir</span></div>
-                    <div class="heading-title">Produk <span class="tx-primary"> Daur Ulang </span> </div>
-                    <div class="heading-description">Dari limbah rumah tangga Anda, menjadi barang bernilai yang layak
-                        pakai.</div>
+<section class="section">
+    <div class="container">
+        <div class="heading-section">
+            <div class="heading-subtitle"><span class="tx-primary tx-16 fw-semibold">Produk Akhir</span></div>
+            <div class="heading-title">Produk <span class="tx-primary"> Daur Ulang </span> </div>
+            <div class="heading-description">Dari limbah rumah tangga Anda, menjadi barang produksi yang
+                bernilai pakai.</div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card mb-lg-0">
+                    <div class="position-relative">
+                        <a href="blog-details.html">
+                            <img class="card-img-top" src="assets/images/blog/pulp_recyloop.jpg" alt="blog-image">
+                        </a>
+                    </div>
+                    <div class="card-body d-flex flex-column">
+                        <h5><a href="blog-details.html" class="tx-color-default">Pulp</a></h5>
+                        <div class="tx-muted">Pulp merupakan bubur serat yang didaur ulang dari kertas dan
+                            kardus untuk diproduksi kembali menjadi produk kertas.</div>
+                    </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card mb-lg-0">
-                            <div class="position-relative">
-                                <a href="blog-details.html">
-                                    <img class="card-img-top" src="assets/images/blog/3.jpg" alt="blog-image">
-                                </a>
-                            </div>
-                            <div class="card-body d-flex flex-column">
-                                <h5><a href="blog-details.html" class="tx-color-default">Pulp</a></h5>
-                                <div class="tx-muted">Pulp atau bubur kertas adalah hasil dari daur ulang kembali buku dan dapat di gunakan kembali menjadi kertas.</div>
-                            </div>
-                        </div>
-                        <!-- COL-END -->
+                <!-- COL-END -->
+            </div>
+            <!-- COL-END -->
+            <div class="col-lg-4">
+                <div class="card mb-lg-0">
+                    <div class="position-relative">
+                        <a href="blog-details.html">
+                            <img class="card-img-top" src="assets/images/blog/alumunium_recyloop.jpg" alt="blog-image">
+                        </a>
                     </div>
-                    <!-- COL-END -->
-                    <div class="col-lg-4">
-                        <div class="card mb-lg-0">
-                            <div class="position-relative">
-                                <a href="blog-details.html">
-                                    <img class="card-img-top" src="assets/images/blog/6.jpg" alt="blog-image">
-                                </a>
-                            </div>
-                            <div class="card-body d-flex flex-column">
-                                <h5><a href="blog-details.html" class="tx-color-default">Alumunium Murni</a></h5>
-                                <div class="tx-muted">Merupakan produk hasil dari daur ulang kaleng yang dapat di gunakan menjadi minuman kaleng atau bisa menjadi handphone.</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card mb-lg-0">
-                            <div class="position-relative">
-                                <a href="blog-details.html">
-                                    <img class="card-img-top" src="assets/images/blog/5.jpg" alt="blog-image">
-                                </a>
-                            </div>
-                            <div class="card-body d-flex flex-column">
-                                <h5><a href="blog-details.html" class="tx-color-default"> Ecobrick
-                                    </a></h5>
-                                <div class="tx-muted">Merupakan produk hasil dari daur ulang plastik dengan menggantikan batu bata konvensional dengan bahan plastik yang dipadatkan hingga benar-benar mengeras.</div>
-                            </div>
-                        </div>
+                    <div class="card-body d-flex flex-column">
+                        <h5><a href="blog-details.html" class="tx-color-default">Alumunium Oksida</a></h5>
+                        <div class="tx-muted">Alumunium Oksida merupakan lembar alumunium murni yang didaur
+                            ulang dari kaleng untuk diproduksi menjadi alat dapur.</div>
                     </div>
                 </div>
             </div>
-        </section>
+            <div class="col-lg-4">
+                <div class="card mb-lg-0">
+                    <div class="position-relative">
+                        <a href="blog-details.html">
+                            <img class="card-img-top" src="assets/images/blog/paving_recyloop.jpg" alt="blog-image">
+                        </a>
+                    </div>
+                    <div class="card-body d-flex flex-column">
+                        <h5><a href="blog-details.html" class="tx-color-default">Paving Blocks
+                            </a></h5>
+                        <div class="tx-muted">Paving merupakan blok plastik yang didaur ulang dari botol untuk
+                            diproduksi kembali menjadi bahan bangunan.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!-- END MAIN-CONTENT -->
 </div>
